ec5f089ec748e0c8b07fc8e65f840effcc3474f7
waqasafridi07 <waqaswc07@gmail.com>
2025-02-03 20:51:19 +0500

    Printing area of circle

---
+++ Area of circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the value of radius : ";
+    cin>>r;
+
+    float A = 3.14159*r*r;
+    cout<<"The area is:"<<A;
+}
