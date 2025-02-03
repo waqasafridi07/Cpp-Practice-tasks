fb67a6d09bc3e166142c88270e12762bf7b46780
waqasafridi07 <waqaswc07@gmail.com>
2025-02-03 21:07:59 +0500

    Area of parallelogram

---
+++ Area of paralellogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float b,h;
+    cout<<"Enter value of base : ";
+    cin>>b;
+    cout<<"Enter the value of height : ";
+    cin>>h;
+
+    float A = b*h;
+    cout<<A;
+}
