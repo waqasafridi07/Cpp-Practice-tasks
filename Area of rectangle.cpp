cd23e6d521b11e3c3285eb31c1f98aa51bed37d3
waqasafridi07 <waqaswc07@gmail.com>
2025-02-03 21:04:24 +0500

    Area of rectangle

---
+++ Area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float l,w;
+    cout<<"Enter length : ";
+    cin>>l;
+    cout<<"Enter width : ";
+    cin>>w;
+
+    float A = l*w;
+    cout<<A;
+}
