d49ac2ba65f9db70aca932426b2a0fb652c20f99
waqasafridi07 <waqaswc07@gmail.com>
2025-02-05 14:53:07 +0500

    Printing completing square

---
+++ completing square.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b,c,x;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    cout<<"Enter the value of b : ";
+    cin>>b;
+    cout<<"Enter the value of c : ";
+    cin>>c;
+    cout<<"Enter the value of x : ";
+    cin>>x;
+
+    if (x<=0){
+        cout<<"No solution, x must be greater than zero"<<endl;
+    }
+    else if(x>1){
+        cout<<x/x;
+    }
+
+    float ab = a* (x + (b/ 2*a));
+    float absquare = ab*ab;
+
+    float result =  (a*absquare) - (((b*b )- (4*a*c))/4*a);
+    cout<<"Result = "<< result;
+}
