5d76454f840ea1fd98790379eb7bcec2da96eb4d
waqasafridi07 <waqaswc07@gmail.com>
2025-02-11 19:44:54 +0500

    Welcoming the user

---
+++ Problem 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string Name;
+    cout<<"What is your name?"<<endl;
+    cin>>Name;
+
+    cout<<"Welcome " << Name <<endl;
+    
+
+}
