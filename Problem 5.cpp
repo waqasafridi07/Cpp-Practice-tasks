42ade23040897715b740ed929de356d3a90b793c
waqasafridi07 <waqaswc07@gmail.com>
2025-02-11 20:26:24 +0500

    Printing size of fundamental data types

---
+++ Problem 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+   cout<<"The size of int data type is : " << sizeof(int) <<" bytes "<<endl;
+   cout<<"The size of char data type is : " << sizeof(char) <<" bytes "<<endl;
+   cout<<"The size of float data type is : " << sizeof(float) <<" bytes "<<endl;
+   cout<<"The size of short data type is : " << sizeof(short) <<" bytes "<<endl;
+   cout<<"The size of long data type is : " << sizeof(long) <<" bytes "<<endl;
+   cout<<"The size of long long data type is : " << sizeof(long long) <<" bytes "<<endl;
+   cout<<"The size of double data type is : " << sizeof(double) <<" bytes "<<endl;
+   cout<<"The size of long double data type is : " << sizeof(long double) <<" bytes "<<endl;
+   cout<<"The size of bool data type is : " << sizeof(bool) <<" bytes ";
+
+}
+
