3b3837a8e9db02821e8f97ac46159df4859a6b07
waqasafridi07 <waqaswc07@gmail.com>
2025-02-17 07:51:21 +0500

    Printing limits

---
+++ Problem 6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    float a, b, c, d, e, f, g, h, i, j, k ,l ,m ,n ,s;
+    a = INT_MIN;
+    b = INT_MAX;
+    c =  UINT_MAX;
+    d = LLONG_MAX;
+    e = LLONG_MIN;
+    f = ULLONG_MAX;
+    g = CHAR_BIT;
+    h = CHAR_MAX;
+    i = CHAR_MIN;
+    j = SCHAR_MAX;
+    k = SCHAR_MIN;
+    l = UCHAR_MAX;
+    m = SHRT_MIN;
+    n = SHRT_MAX;
+    s = USHRT_MAX;
+
+    cout<<"INT_MIN " << a <<endl;
+    cout<<"INT_MAX " << b <<endl;
+    cout<<"UINT_MAX " << c <<endl;
+    cout<<"LLONG_MAX " << d <<endl;
+    cout<<"LLONG_MIN " << e <<endl;
+    cout<<"ULLONG_MAX " << f <<endl;
+    cout<<"CHAR_BIT " << g <<endl;
+    cout<<"CHAR_MAX " << h <<endl;
+    cout<<"CHAR_MIN " << i <<endl;
+    cout<<"SCHAR_MAX " << j <<endl;
+    cout<<"INT_MIN " << k <<endl;
+    cout<<"UCHAR_MAX " << l <<endl;
+    cout<<"SHRT_MIN " << m <<endl;
+    cout<<"SHRT_MAX " << n <<endl;
+    cout<<"USHRT_MAX " << s <<endl;
+}
