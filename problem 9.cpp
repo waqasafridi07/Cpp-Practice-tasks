37acd92782c872dd138c622970cc7a931541c71c
waqasafridi07 <waqaswc07@gmail.com>
2025-02-17 08:36:07 +0500

    post and pre increment/decrement

---
+++ problem 9.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 57;
+    cout<<a<<endl;
+    a++;
+    cout<<a<<endl;
+
+    int b = 58;
+
+    ++b;
+    cout<<b<<endl;
+
+    int c = 59;
+    cout<<59+1<<endl;
+
+    int d = 60;
+    d--;
+    cout<<d<<endl;
+
+    int e = 59;
+    --e;
+    cout<<e<<endl;
+
+    int f = 58;
+    cout<<58-1;
+}
