ee8409dce66158d1b61cbc477d5be745c0aa2fe2
waqasafridi07 <waqaswc07@gmail.com>
2025-02-17 08:13:48 +0500

    printing mixed data types and arithmetic operations

---
+++ problem 7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int a, b;
+    a = 5;
+    b = 7;
+
+    float c, d;
+    c = 3.7;
+    d = 8.0;
+
+    cout<<"5 + 7 = " << 5 + 7 <<endl;
+    cout<<"3.7 + 8.0 = " << 3.7 + 8.0 <<endl;
+    cout<<"5 + 8.0 = " << 5 + 8.0 <<endl;
+    cout<<"5 - 7 = " << 5 - 7 <<endl;
+    cout<<"3.7 - 8.0 = " << 3.7 - 8.0 <<endl;
+    cout<<"5 - 8.0 = " << 5 - 8.0 <<endl;
+    cout<<"5 * 7 = " << 5 * 7 <<endl;
+    cout<<"3.7 * 8.0 = " << 3.7 * 8.0 <<endl;
+    cout<<"5 * 8.0 = " << 5 * 8.0 <<endl;
+    cout<<"5 / 7 = " << 5 / 7 <<endl;
+    cout<<"3.7 / 8.0 = " << 3.7 / 8.0 <<endl;
+    cout<<"5 / 8.0 = " << 5 / 8.0 <<endl;
+}
